d257a53b106f5f2322aa9cba1c27dce044342e6c
ccryptoDev <smartwebdev90@gmail.com>
2024-04-07 13:31:56 +0200

    updated phone view and LED respolution

--- src/resources/views/dashboard/send-to-sign.blade.php
+++ src/resources/views/dashboard/send-to-sign.blade.php
@@ -2,41 +2,61 @@
 <!-- Aside Container -->
 <!-- full background -->
 <style>
-    /* .form-control {
-        border: 1px solid #007bff;
-    } */
+    .form-control {
+        font-size: 14px;
+    }
+    label {
+        font-size: 14px;
+    }
 </style>
 <div class="fluid bg-white">
     <!-- page outer -->
     <div class="d-flex overflow-hidden">
-        <div class="d-flex flex-column justify-content-betfween px-8 py-10 px-lg-24">
-            <!-- custom header -->
-            <div class="custom-header">
+        <div class="d-flex flex-column justify-content-betfween px-8 pt-10 pb-5 px-lg-24">
+            <!-- custom header  -->
+            <div class="custom-header px-4 px-md-16">
                 <div class="page-logo">
-                    <img src="/assets/media/logos/logo_new.png" class="login-header-logo-image" alt="" />
+                    <img  src="/assets/media/logos/logo_new.png" class="login-header-logo-image" alt=""  /> 
                 </div>
-                <div class="text-center italic page-title">
-                    <h2>Choose a Message</h2>
-                    <p>Search for a message by keyword, then use slider or thumbnails to choose an image, click on
-                        middle image in the bluebox to choose</p>
+                <div class="d-none d-sm-block text-center italic page-title">
+                    <h2>Work with a Message</h2>
+                    <p>This menu allows the user to retrieve or edit existing messages, make new ones or send to the sign</p>
                 </div>
                 <div class="qrcode-form">
                     <a href="#">Click for HELP</a>
                     <img src="/assets/media/mainmenu/qr_code.png" alt="Sign Controller QRcode">
                 </div>
             </div>
+
+            <div class="px-4">
+                <div class="d-block d-sm-none text-center italic page-title">
+                    <h2>Work with a Message</h2>
+                    <p>This menu allows the user to retrieve or edit existing messages, make new ones or send to the sign</p>
+                </div>
+            </div>  
             <!-- end: custom-header -->
 
             <!-- begin::Aside body -->
             <div class="d-flex flex-column-fluid flex-column px-16 page-container message-menu">
                 <div class="main-menu">
                     <div class="search-item">
-                        <label class="italic">Keywords: </label>
-                        <input class="form-control search-input text-center" name="id-name" value="1002-35MPH"></input>
+                        <!-- <label class="italic">Information: </label> -->
+                        <input class="form-control search-input text-center"
+                            id="information"
+                            value=""
+                            placeholder="The information of selected sign will be displayed here" 
+                            disabled
+                        >
+                        </input>
                     </div>
                     <div class="search-item">
-                        <label class="italic">Search 1: </label>
-                        <input class="form-control search-input text-center" name="keyword" id="firstSearch" ></input>
+                        <!-- <label class="">Search 1: </label> -->
+                        <input class="form-control search-input" 
+                            name="keyword" 
+                            id="firstSearch" 
+                            placeholder="Please type first keyword"
+                        >
+                        </input>
                     </div>
                 </div>
             </div>
@@ -45,31 +65,30 @@
     </div>
 </div>
 </div>
-<div id="slickPanel">
-    <!-- <div class="slick">
-        @foreach ($images as $image)
-        <div>
-            <span>
-                <img src="{{ asset('assets/media/signmessage/' . $image) }}" alt="image" />
-            </span>
-        </div>
-        @endforeach
-        
-    </div> -->
-</div>
+<div id="slickPanel" class="slick-panel"></div>
 <div class="fluid bg-white">
     <!-- page outer -->
-    <div class="d-flex flex-column-fluid flex-column px-16 page-container message-menu">
-        <div class="main-menu">
-            <div class="search-item">
-                <label class="">Search 2: </label>
-                <input class="form-control search-input text-center" name="id-name" id="secondSearch" ></input>
+    <div class="d-flex flex-column justify-content-between px-8 pt-10 pb-0 px-lg-24">
+        <div class="px-16">
+            <div class="main-menu">
+                <div class="search-item">
+                    <!-- <label class="">Search 2: </label> -->
+                    <input class="form-control search-input"
+                        name="id-name"
+                        id="secondSearch" 
+                        placeholder="Please type second keyword"
+                    >
+                    </input>
+                </div>
             </div>
         </div>
     </div>
-    <div class="d-flex overflow-hidden">
+
+            
+        
+    <!-- <div class="d-flex overflow-hidden"> -->
         <div class="d-flex flex-column justify-content-betfween px-8 py-lg-10 px-lg-24">
-            <div class="d-flex flex-column-fluid flex-column px-16 page-container message-menu">
+            <div class="d-flex flex-column-fluid flex-column px-12 page-container message-menu">
                 <!-- <div class="main-menu">
                     <div class="search-item">
                         <label class="visible-hidden">Search: </label>
@@ -90,28 +109,33 @@
     </div>
     <div class="d-flex flex-column-fluid flex-column px-16 page-container message-menu">
         <div class="main-menu">
-            <div class="search-item">            
-                <button class="btn btn-danger mt-0 d-inline mr-3 mt-10" type="button" id="send">Send</button>
+            <div class="">            
+                <button class="btn btn-danger d-inline my-10" type="button" id="send">Send</button>
             </div>
         </div>
     </div>
-</div>
+<!-- </div> -->
 
 <script src="/assets/js/messagesign.js"></script>
 <script src="/assets/js/redirect.js"></script>
 <script>
-    var currentIndex = 0;
-    var images = {!! json_encode($images) !!};
     
+    var images = {!! json_encode($images) !!};
+    images = images.map((image, index) => ({ id: index, name: image }) );
+    
+    var firstIndex = 0, secondIndex = 0;
+    var firstSelectedImages = [], secondSelectedImages = [];
+
     $("#send").on("click", function () {
         event.preventDefault();
 
-        if (images[currentIndex]) {
+        if (firstSelectedImages[firstIndex] && firstSelectedImages[firstIndex].id && images[firstSelectedImages[firstIndex].id]) {
+
             $.ajax({
                 url : '/send-image-socket',
                 type : "POST",
                 data : {
-                    imageName: images[currentIndex],
+                    imageName: images[firstSelectedImages[firstIndex].id].name,
                 },
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -136,12 +160,12 @@
 
     var secondSearch  = function () {
         var value1 = $("#firstSearch").val().toLowerCase().trim(), value2 = $("#secondSearch").val().toLowerCase().trim();
-        var secondSelectedImages = images.filter(image => image.toLowerCase().trim().includes(value1) && image.toLowerCase().trim().includes(value2));
+        secondSelectedImages = images.filter(image => image.name.toLowerCase().trim().includes(value1) && image.name.toLowerCase().trim().includes(value2));
 
         $("#thumbnail-list").html('');
         for (var i = 0; i < secondSelectedImages.length; i++) {
 
-            var component = `<li><span><img src="{{ asset('assets/media/signmessage/${secondSelectedImages[i]}' ) }}" alt="image" /></span></li>`;
+            var component = `<li><span><img src="{{ asset('assets/media/signmessage/${ secondSelectedImages[i].name }' ) }}" alt="image" /></span></li>`;
             $("#thumbnail-list").append(component);
 
         }
@@ -152,27 +176,44 @@
     $("#firstSearch").on('keyup', function(e){
         
         var value = $("#firstSearch").val().toLowerCase().trim();
-        var firstSelectedImages = images.filter(image => image.toLowerCase().trim().includes(value));
+        firstSelectedImages = images.filter(image => image.name.toLowerCase().trim().includes(value));
 
         // console.log(firstSelectedImages);
 
         $("#slickPanel").html('<div class="slick" id="slick"></div>');
         for (var i = 0; i < firstSelectedImages.length; i++) {
 
-            var component = `<div><span><img src="{{ asset('assets/media/signmessage/${firstSelectedImages[i]}') }}" alt="image" /></span></div>`;
+            var component = `<div><span><img src="{{ asset('assets/media/signmessage/${firstSelectedImages[i].name}') }}" alt="image" /></span></div>`;
             $("#slick").append(component);
 
         }
 
-        slickFunction();
+        // initialization
+        firstIndex = 0;
+        if (firstSelectedImages.length > 0) {
+            var name = firstSelectedImages[firstIndex].name;
+            $("#information").val(name);
+        } else {
+            $("#information").val("");
+        }
 
+        slickFunction();
         secondSearch();
     });
 
     $("#secondSearch").on('keyup', function(e){
 
+        
         secondSearch();        
-
+        
+        secondIndex = 0;
+        if (secondSelectedImages.length > 0) {
+            var name = secondSelectedImages[secondIndex].name;
+            // .split(".bmp")[0].split("_").join(" ");
+            $("#information").val(name);
+        } else {
+            $("#information").val("");
+        }
     });
 
 
