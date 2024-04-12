65e27c691ad7bb2ac979be465056d94359b2c41c
Inextom <155579018+Inextom@users.noreply.github.com>
2024-04-11 23:02:38 -0500

    lsop i

--- src/app/Http/Controllers/SocketController.php
+++ src/app/Http/Controllers/SocketController.php
@@ -139,7 +139,7 @@ class SocketController extends Controller {
         echo '____Original Data-' . $data ."<br>\n"; 
         echo '__Compressed Data-' . bin2hex($compressedData) . "<br>\n";
         echo '------------------------------' ."<br>\n";
-        $decompressedData = exec("echo '$compressedData' | lzop -d -c");
+       // $decompressedData = exec("echo '$compressedData' | lzop -d -c");
        // echo '__Compressed Data-' ;
        // echo  $compressedData ; 
        // echo 'Decompressed Data-' ;
