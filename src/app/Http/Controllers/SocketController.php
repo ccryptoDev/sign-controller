741080bfedb4790e54e1f14fde7b4603580cf835
Inextom <155579018+Inextom@users.noreply.github.com>
2024-04-15 08:29:23 -0500

    lzo4

--- src/app/Http/Controllers/SocketController.php
+++ src/app/Http/Controllers/SocketController.php
@@ -202,6 +202,7 @@ class SocketController extends Controller {
          file_put_contents($tempfiletodecompress,$DataToDecompress) ;
       
          exec("ls -la $tempdecompressedfile")
+        exec("sleep 10")
 
       // use the Command line lzop, to open the file, compress, write to another file:
         shell_exec("/usr/bin/lzop $tempfiletodecompress -d -o $tempdecompressedfile");
