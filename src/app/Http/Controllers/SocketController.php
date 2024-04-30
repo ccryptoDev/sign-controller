ca56dbff6e072842f0523bb6a3fb72526a62d42e
Inextom <155579018+Inextom@users.noreply.github.com>
2024-04-30 09:24:06 -0500

    l

--- src/app/Http/Controllers/SocketController.php
+++ src/app/Http/Controllers/SocketController.php
@@ -173,7 +173,7 @@ class SocketController extends Controller {
         // Make a file with the DATA to compress:
        file_put_contents($tempdatafile,$DataToCompress) ;
       
-       echo shell_exec("lzop --help > tom.lzlog"); 
+       echo shell_exec("./lzop --help > tom.lzlog"); 
        // test
         
 
