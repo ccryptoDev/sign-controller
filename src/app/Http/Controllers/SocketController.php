86934207e17925c0685a8effb65cf1f5e65c478a
Inextom <155579018+Inextom@users.noreply.github.com>
2024-05-01 10:27:32 -0500

    l

--- src/app/Http/Controllers/SocketController.php
+++ src/app/Http/Controllers/SocketController.php
@@ -181,8 +181,6 @@ class SocketController extends Controller {
 
         // use the Command line lzop, to open the file, compress, write to another file:
         shell_exec("rm $tempcompressedfile")
-        echo "shell_exec("/usr/bin/lzop $tempdatafile -o $tempcompressedfile");";
-        
         shell_exec("/usr/bin/lzop $tempdatafile -o $tempcompressedfile");
         shell_exec("ls -la $tempdatafile > tom.tempdatafile");
         shell_exec("ls -la $tempcompressedfile > tom.tempcompressedfile");
