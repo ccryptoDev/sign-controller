e3165770cf63ac0fb3863f70eae2b89caa275506
Inextom <155579018+Inextom@users.noreply.github.com>
2024-05-01 14:22:26 -0500

    l

--- src/app/Http/Controllers/SocketController.php
+++ src/app/Http/Controllers/SocketController.php
@@ -208,7 +208,7 @@ class SocketController extends Controller {
         $compressedData = "" ;
       
         // Read back in the compressed data:
-        shell_exec("echo -la $tempcompressedfile* > tom.tempcompressedfile");
+        shell_exec("echo -la $tempcompressedfile* > tom.tempcmd");
         shell_exec("ls -la $tempcompressedfile* > tom.tempcompressedfile");
         sleep(5) ;
         $compressedData = file_get_contents($tempcompressedfile) ;
