7616dbb04ef531a474c8f9b5a5895a02e6ca9a86
ccryptoDev <smartwebdev90@gmail.com>
2024-05-06 08:34:14 +0200

    added more logs

--- start_docker_compose.sh
+++ start_docker_compose.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/docker-compose -f /home/inexadmin/sign-controller/docker-compose.yml up -d --build app --verbose
+/usr/bin/docker-compose -f /home/inexadmin/sign-controller/docker-compose.yml up -d --build app --log-level=DEBUG
